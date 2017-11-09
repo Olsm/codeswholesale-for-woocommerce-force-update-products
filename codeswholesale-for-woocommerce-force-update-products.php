259a609f04c5673d0d1c36cc801e540a2dc1ca10
OlavOlsm <OlavOlsm@gmail.com>
2017-11-09 21:15:31 +0100

    add functionality for force updat product price and stock

---
+++ codeswholesale-for-woocommerce-force-update-products.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: CodesWholesale Force update products price and stock
+Description: Updates all products price and stock in Woocommerce from CodesWholesale
+Version: 1.0.0
+Author: Olav Småriset
+*/
+
+use CodesWholesaleFramework\Postback\UpdatePriceAndStock\UpdatePriceAndStockAction;
+
+add_action("activated_plugin", 'force_update_price_and_stock');
+
+function force_update_price_and_stock() {
+    $products = wc_get_products(['status' => 'publish']);
+    foreach ($products as $product) {
+        set_time_limit(10);
+        update_price_and_stock_by_post_id($product->id);
+    }
+    deactivate_plugins( plugin_basename( __FILE__ ) );
+}
+
+/**
+ * @param int $post_id
+ */
+function update_price_and_stock_by_post_id($post_id) {
+    $productId = get_post_meta($post_id, CodesWholesaleConst::PRODUCT_CODESWHOLESALE_ID_PROP_NAME, true);
+
+    if (!empty($productId)) {
+        $action = new UpdatePriceAndStockAction(new WP_Update_Price_And_Stock(), new WP_Spread_Retriever());
+        $action->setConnection(CW()->get_codes_wholesale_client());
+        $action->setProductId($productId);
+        $action->process();
+    }
+}
